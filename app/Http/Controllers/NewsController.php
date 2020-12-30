66dc69cb1f9d20c8ce76a7f3845fc961883f2a35
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2020-12-30 14:23:31 +0300

    Cleaned up the code

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -10,46 +10,13 @@ class NewsController extends Controller
 {
     public function index()
     {
-        // dd(DB);
-
-       /* $sql = "SELECT * FROM categories";
-        // $result = D::select($sql);
-        // $result = DB::statement($sql);
-        // dump($result);
-        $result = DB::select($sql); */
-
-        // $categories = (new Category())->getCategoriesList();
-
-        $categories = [];
-
-
-        
-
-     /*   $sql = "SELECT * FROM categories WHERE id = :id";
-        $result = DB::select($sql, [':id' => 2]);
-         dd($result); */
-
-      /*   $result = DB::table('categories')
-            ->where(["id" => 3])
-            ->get(); */
-
-            // $result = DB::table('news')
-            //     ->get();
-
         $result = DB::table('categories')
             ->get();
 
+        $categories = [];
         foreach ($result as $item) {
-            // dump($item);
-            // dump($item->name);
             $categories[] = $item->name;
         }
-
-        // dd($result->toArray());
-
-        // dump($categories);
-        // dd($categories);
-        // dump($categories);
         return view(
             'news.index', 
             ['categories' => $categories]
@@ -60,27 +27,6 @@ class NewsController extends Controller
     {
         $sql = 'SELECT * FROM news WHERE category_id = :categoryId';
         $news = DB::select($sql, [':categoryId' => $categoryId]);
-        // $result = DB::table('news')
-        //     ->where(['category_id' => $categoryId])
-        //     ->get();
-
-      /*  $news = [];
-        foreach ($result as $id => $item) {
-            // dump($item);
-            // dump($item->name);
-            $news[$id + 1] = $item;
-        //     $news[$id + 1] = $item->title;
-        } */
-
-        // dd($news);
-
-        // foreach ($this->news as $id => $item) {
-        //     if($item['category_id'] == $categoryId) {
-        //         $news[$id + 1] = $item;
-        //     }
-        // }
-
-        // $news = (new News())->getByCategoryId($categoryId);
         return view(
             'news.category', 
             ['news' => $news]
@@ -91,8 +37,6 @@ class NewsController extends Controller
     {
         $sql = 'SELECT * FROM news WHERE id = :id';
         $newsOne = DB::select($sql, [':id' => $id]);
-        // dd($newsOne);
-        // $news = (new News())->getById($id);
         return view(
             'news.item', 
             ['newsOne' => $newsOne]

cce3bafc023c99d1f24a1a69e922b811a0939331
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2021-01-02 21:07:17 +0300

    Updated styles

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,14 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@section('title')Page @show</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <!-- <link rel="stylesheet" href="{{asset('css/app.css')}}"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
 </head>
 <body>
-    @include('blocks.header')
-
     <div class="content">
+        @include('blocks.header')
         @yield('content')
     </div>
 

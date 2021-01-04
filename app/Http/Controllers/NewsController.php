0331c03a97bafc04c3137b7dde0606d7faf899a7
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2021-01-04 11:26:17 +0300

    Removed unnecessary comments

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -7,7 +7,6 @@ use App\Models\Category;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
-// use Illuminate\Support\Facades\DB;
 
 class NewsController extends Controller
 {
@@ -23,14 +22,6 @@ class NewsController extends Controller
 
     public function categories()
     {
-        // dd(News::all());
-        // $result = DB::table('categories')
-        //     ->get();
-
-        // $categories = [];
-        // foreach ($result as $item) {
-        //     $categories[] = $item->name;
-        // }
         $categories = Category::query()
             ->get();
         return view(
@@ -41,15 +32,7 @@ class NewsController extends Controller
 
     public function getByCategory($categoryId)
     {
-        // dd(News::all());
-
-        // $sql = 'SELECT * FROM news WHERE category_id = :categoryId';
-        // $news = DB::select($sql, [':categoryId' => $categoryId]);
-
         $news = (new News())->getByCategoryId($categoryId);
-
-        // dump($news);
-
         return view(
             'news.category', 
             ['news' => $news]
@@ -58,8 +41,6 @@ class NewsController extends Controller
 
     public function renderNews($id)
     {
-        // $sql = 'SELECT * FROM news WHERE id = :id';
-        // $newsOne = DB::select($sql, [':id' => $id]);
         $newsOne = News::find($id);
         return view(
             'news.item', 
